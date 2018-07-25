d89669150b2b9cc7bf47e3a667d3d473ffd65c3c
zombie24x7 <androidshubham@gmail.com>
2018-07-25 19:10:48 +0530

    data_structure

---
+++ data_structure/stack_linkedlist_calendar.js
@@ -0,0 +1,44 @@
+class Node {
+	constructor(data) {
+        this.data=data;
+        this.next=null;
+    }
+}
+
+    var head;
+	exports.push = (data) => {
+        var n = new Node(data);
+		if(head==null)
+			head=n;
+		else {
+			n.next=head;
+			head=n;
+		}
+	}
+	
+	exports.display = () => {
+		var t=head;
+		while(t!=null) {
+			console.log(t.data);
+			t=t.next;
+		}
+	}
+	
+	exports.remove = () => {
+		var t=head;
+		head = t.next;
+	}
+	exports.pop = () => {
+		var t=head;
+		var temp = t.data;
+		head = t.next;
+		return temp;
+    }
+    
+    exports.isEmpty = () => {
+        if(head == null){
+            return true;
+        } else {
+            return false;
+        }
+    }
